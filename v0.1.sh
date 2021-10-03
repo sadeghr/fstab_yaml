2fc8f18bbe6ae738802220e28ad9348d1475b1ca
Sadegh Rashidi <sadegh.rashidi@gmail.com>
2021-10-03 20:05:22 +0330

    Add files via upload

---
+++ v0.1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Yaml input file
+yamlfile=fstab.yaml
+
+#All Mount Point Count
+mountpoints=`yq eval '.fstab | keys' $yamlfile | wc -l`
+
+for ((i=1 ; i <=$mountpoints ; i++)) ;
+do
+	device='"'`yq eval '.fstab | keys | join(" ")' $yamlfile |  awk '{print $'$i'}'`'"';
+	mount=`yq eval '.fstab | keys | join(" ")' $yamlfile |  awk '{print $'$i'}'`
+	export=':'`yq eval ".fstab.$device.export" $yamlfile`
+	mountpoint=`yq eval ".fstab.$device.mount" $yamlfile`
+	fstype=`yq eval ".fstab.$device.type" $yamlfile`
+	options=`yq eval ".fstab.$device.options" $yamlfile`
+echo -e $mount$export'\t'$mountpoint'\t'$fstype'\t'defaults$options'\t'0'\t'0 | sed -e s/":null"//g -e s/"null"//g -e s/"- "/,/g ;
+done;
