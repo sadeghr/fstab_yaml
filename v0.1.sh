ac0000a83bc1b7490c5c69b4074fc78383c30f00
Sadegh Rashidi <sadegh.rashidi@gmail.com>
2021-10-04 00:40:29 +0330

    Update v0.1.sh

--- v0.1.sh
+++ v0.1.sh
@@ -8,11 +8,13 @@ mountpoints=`yq eval '.fstab | keys' $yamlfile | wc -l`
 
 for ((i=1 ; i <=$mountpoints ; i++)) ;
 do
-	device='"'`yq eval '.fstab | keys | join(" ")' $yamlfile |  awk '{print $'$i'}'`'"';
-	mount=`yq eval '.fstab | keys | join(" ")' $yamlfile |  awk '{print $'$i'}'`
-	export=':'`yq eval ".fstab.$device.export" $yamlfile`
-	mountpoint=`yq eval ".fstab.$device.mount" $yamlfile`
-	fstype=`yq eval ".fstab.$device.type" $yamlfile`
-	options=`yq eval ".fstab.$device.options" $yamlfile`
-echo -e $mount$export'\t'$mountpoint'\t'$fstype'\t'defaults$options'\t'0'\t'0 | sed -e s/":null"//g -e s/"null"//g -e s/"- "/,/g ;
+
+device='"'`yq eval '.fstab | keys | join(" ")' $yamlfile |  awk '{print $'$i'}'`'"';
+mount=`yq eval '.fstab | keys | join(" ")' $yamlfile |  awk '{print $'$i'}'`
+export=':'`yq eval '.fstab.'$device'.export' $yamlfile`
+mountpoint=`yq eval '.fstab.'$device'.mount' $yamlfile`
+fstype=`yq eval '.fstab.'$device'.type' $yamlfile`
+options=`yq eval '.fstab.'$device'.options | join(",")' $yamlfile  2>/dev/null`
+
+echo -e $mount$export'\t'$mountpoint'\t'$fstype'\t'defaults,$options'\t'0'\t'0 | sed -e s/":null"//g;
 done;
